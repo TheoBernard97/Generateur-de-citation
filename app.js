0fe16501e9fae19fc4c1c2db0bf1816530d890de
Théo Bernard <theo.bernard.94100@gmail.com>
2021-12-17 11:55:57 +0100

    🚧 Setup some stuff

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 
 import quotes from "./assets/quotes.js";
 
+let quoteQuantity = 1;
+const minQuoteQuantity = 1;
+const maxQuoteQuantity = 5;
+
 const btn = document.getElementById("btn");
 const displayedQuote = document.getElementById("result");
 btn.addEventListener("click", () => {
@@ -12,12 +16,21 @@ btn.addEventListener("click", () => {
 function quoteGenerator() {
   let fullQuote = "";
 
-  let startingQuote = quotes.stating_quotes[getRandomInt(10)];
-  let middleQuote = quotes.middle_quotes[getRandomInt(10)];
-  let endingQuote = quotes.ending_quotes[getRandomInt(10)];
+  let startingQuote = quotes.stating_quotes[getRandomInt(quotes.stating_quotes.length)];
+  let middleQuote = quotes.middle_quotes[getRandomInt(quotes.middle_quotes.length)];
+  let endingQuote = quotes.ending_quotes[getRandomInt(quotes.ending_quotes.length)];
 
   fullQuote = `${startingQuote} ${middleQuote} ${endingQuote}.`;
   return fullQuote;
+}
+
+function updateSettings(input) {
+  numberOfQuotesToGenerate = input;
+  displayNewSettings();
+}
+
+function displayNewSettings() {
+  
 }
 
 function getRandomInt(max) {
