8d7d8de4e5a583025f167fdf5cfcebc7aa26503a
Théo Bernard <theo.bernard@moonshot-internet.com>
2021-12-18 11:11:54 +0100

    ✨ Setup settings + Refacto

--- app.js
+++ app.js
@@ -8,29 +8,51 @@ const maxQuoteQuantity = 5;
 
 const btn = document.getElementById("btn");
 const displayedQuote = document.getElementById("result");
-btn.addEventListener("click", () => {
-  let fullQuote = quoteGenerator();
-  displayedQuote.innerHTML = fullQuote;
-});
+const numberOfQuotes = document.getElementById("quote-quantity");
+const incrementQuotes = document.getElementById("more");
+const decrementQuotes = document.getElementById("less");
+
+setupEventListeners();
+
+function setupEventListeners() {
+  document.addEventListener("click", (event) => {
+    if (event.target === btn) {
+      const fullQuote = quoteGenerator();
+      displayQuotes(fullQuote);
+    }
+    if (event.target === incrementQuotes && maxQuoteQuantity > quoteQuantity) {
+      console.log("+");
+      quoteQuantity++;
+      updateSettings();
+    }
+    if (event.target === decrementQuotes && minQuoteQuantity < quoteQuantity) {
+      quoteQuantity--;
+      updateSettings();
+    }
+  });
+}
 
 function quoteGenerator() {
   let fullQuote = "";
 
-  let startingQuote = quotes.stating_quotes[getRandomInt(quotes.stating_quotes.length)];
-  let middleQuote = quotes.middle_quotes[getRandomInt(quotes.middle_quotes.length)];
-  let endingQuote = quotes.ending_quotes[getRandomInt(quotes.ending_quotes.length)];
+  let startingQuote =
+    quotes.stating_quotes[getRandomInt(quotes.stating_quotes.length)];
+  let middleQuote =
+    quotes.middle_quotes[getRandomInt(quotes.middle_quotes.length)];
+  let endingQuote =
+    quotes.ending_quotes[getRandomInt(quotes.ending_quotes.length)];
 
   fullQuote = `${startingQuote} ${middleQuote} ${endingQuote}.`;
   return fullQuote;
 }
 
-function updateSettings(input) {
-  numberOfQuotesToGenerate = input;
-  displayNewSettings();
+function displayQuotes(value) {
+  displayedQuote.innerHTML = value;
 }
 
-function displayNewSettings() {
-  
+function updateSettings() {
+  console.log("updateSettings");
+  numberOfQuotes.innerHTML = quoteQuantity;
 }
 
 function getRandomInt(max) {
